f5124a90b17ba0377a9ead59105d9db572d00021
zoeym <zoeym@10.101.120.72>
2024-03-07 12:43:28 +0800

    Rename project path from HR_Payroll_System to HRPayrollSystem for consistency with previous files

---
+++ HRPayrollSystem/src/hrpayrollsystem/Client.java
@@ -0,0 +1,16 @@
+package hrpayrollsystem;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class Client {
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        //Interface hrInterface = (Interface)Naming.lookup("rmi://localhost:1040/HrServer");
+        Interface hrInterface = null;
+
+        AdminPage adminPage = new AdminPage(hrInterface);
+        adminPage.setVisible(true);
+    }
+}
---
+++ HRPayrollSystem/src/hrpayrollsystem/Interface.java
@@ -0,0 +1,7 @@
+package hrpayrollsystem;
+
+import java.rmi.Remote;
+
+public interface Interface extends Remote {
+    
+}
